aedc0537826cc4957b7da37115d0c76da037326b
Ning22333 <2936740697@qq.com>
2025-11-17 15:00:21 +0800

    对不带哨兵结点的单链表进行修改，使得其变为带哨兵的单链表

--- 单向链表/底层代码/SinglyLinkedListSentinel.java
+++ 单向链表/底层代码/SinglyLinkedListSentinel.java
@@ -3,8 +3,8 @@ package 单向链表.底层代码;
 import java.util.Iterator;
 import java.util.function.Consumer;
 
-public class SinglyLinkedList implements Iterable<Integer>{
-    Node head=null;//头指针
+public class SinglyLinkedListSentinel implements Iterable<Integer>{
+    private Node head=new Node(666,null);//带哨兵的头指针
     //添加static的时机：类内部的元素与外界无关联就可加上static
     private static class Node{
         int value;
@@ -15,22 +15,26 @@ public class SinglyLinkedList implements Iterable<Integer>{
         }
     }
     public void addFirst(int value) {
-        //2.代码合并
-        //1.代码分析情况
-//        //1.链表为空
-//        head = new Node(value,null);
-        //2.链表非空
-        head=new Node(value,head);//先把新创建的结点指向最后一个结点防止地址信息丢失，再将头指针指向新创建的结点
+//        //2.代码合并
+//        //1.代码分析情况
+////        //1.链表为空
+////        head = new Node(value,null);
+//        //2.链表非空
+//        head=new Node(value,head);//先把新创建的结点指向最后一个结点防止地址信息丢失，再将头指针指向新创建的结点
+        //改动五：合并，由于insert改变后作业包括addFirst
+        insert(0,value);
     }
     public void loop1(Consumer<Integer> consumer){
-        Node p=head;
+        //改动三
+        Node p=head.next;
         while(p!=null){
             consumer.accept(p.value);
             p=p.next;
         }
     }
     public void loop2(){
-        Node p=head;
+        //改动三
+        Node p=head.next;
         for(;p!=null;p=p.next){
             System.out.println(p.value);
         }
@@ -38,7 +42,8 @@ public class SinglyLinkedList implements Iterable<Integer>{
     @Override
     public Iterator<Integer> iterator() {
         return new Iterator<Integer>() {
-            Node p=head;
+//          改动三：
+            Node p=head.next;
             @Override
             public Integer next() {
                 int v=p.value;
@@ -52,9 +57,10 @@ public class SinglyLinkedList implements Iterable<Integer>{
         };
     }
     private  Node findLast(){
-        if(head==null){
-            return null;
-        }
+        //改动二
+//        if(head==null){
+//            return null;
+//        }
         Node p;
         for (p=head;p.next!=null;p=p.next){
         }
@@ -62,16 +68,20 @@ public class SinglyLinkedList implements Iterable<Integer>{
     }
     public void addLast(int value){
         Node p=findLast();
-        if(p==null){
-            addFirst(value);
-            return;
-        }
+        //改动一
+//        if(p==null){
+//            addFirst(value);
+//            return;
+//        }
         Node newNode=new Node(value,null);
         p.next=newNode;
     }
     public Node findNode(int index){
-        int i=0;
-        for(Node p=head;p!=null;p=p.next,i++){
+        int i=-1;//0修改1为-1表示哨兵
+        //改动三：
+        //改动三忽略了哨兵结点的情况
+        //改动四，因此p应该指向哨兵
+        for(Node p=head/*head.next*/;p!=null;p=p.next,i++){
             if(i==index){
                 return p;
             }
@@ -88,10 +98,12 @@ public class SinglyLinkedList implements Iterable<Integer>{
     }
     public void insert(int index,int value){
         //考虑链表为空
-        if (index==0){
-            addFirst(value);
-            return;
-        }
+        //改动四：
+//        if (index==0){
+//            addFirst(value);
+//            return;
+//        }
+        //若是从索引0位置插入，原代码未考虑哨兵为-1的情况，需要修改findNode
         Node prev=findNode(index-1);
         //考虑索引不存在
         if(prev==null){
@@ -100,17 +112,21 @@ public class SinglyLinkedList implements Iterable<Integer>{
         prev.next=new Node(value,prev.next);
     }
     public void removeFirst(){
-        if(head==null){
-            throw IllegalIndex(0);
-        }
-        head=head.next;
+        //改动六：
+        //由于哨兵存在，不需要谈论头指针为空的情况
+//        if(head==null){
+//            throw IllegalIndex(0);
+//        }
+//        head=head.next;
+        remove(0);
     }
     public void remove(int index){
         //index为0时单独谈论
-        if(index==0){
-            removeFirst();
-            return;
-        }
+        //由于哨兵结点的存在且findNode在之前已经改造完成因此index为零得情况不用单独谈论
+//        if(index==0){
+//            removeFirst();
+//            return;
+//        }
         Node prev=findNode(index-1);
         //没有前继结点时
         if(prev==null){
@@ -129,4 +145,3 @@ public class SinglyLinkedList implements Iterable<Integer>{
         );
     }
 }
-
--- 单向链表/底层代码/test.java
+++ 单向链表/底层代码/test.java
@@ -2,6 +2,9 @@ package 单向链表.底层代码;
 
 public class test {
     public static void main(String[] args) {
+         //不带哨兵的单链表
+        //头插法
+        System.out.println("不带哨兵的单链表");
         SinglyLinkedList list = new SinglyLinkedList();
         list.addFirst(1);
         list.addFirst(2);
@@ -10,13 +13,13 @@ public class test {
         list.loop1(element->{
             System.out.println(element);
         });
-        System.out.println("------------------------");
+        System.out.println("------------------------一般循环遍历");
         list.loop2();
-        System.out.println("------------------------");
+        System.out.println("------------------------迭代遍历");
         for(Integer i : list) {
             System.out.println(i);
         }
-        System.out.println("------------------------");
+        System.out.println("------------------------尾插法测试");
         //尾插法测试
         SinglyLinkedList list1 = new SinglyLinkedList();
         list1.addLast(1);
@@ -27,8 +30,80 @@ public class test {
             System.out.println(element);
         });
         //查找测试
-        System.out.println("------------------------");
+        System.out.println("------------------------查找测试");
         System.out.println(list1.findNode(2));;
         System.out.println(list1.get(3));
+        //插入结点
+        System.out.println("------------------------插入结点");
+        list1.insert(3,10);
+        list1.insert(0,20);
+        list1.loop1(element->{
+            System.out.println(element);
+        });
+        //删除头结点
+        System.out.println("------------------------删除头结点");
+        list1.removeFirst();
+        list1.loop1(element->{
+            System.out.println(element);
+        });
+        //删除指定结点
+        System.out.println("------------------------删除指定结点");
+        list1.remove(1);
+        list1.loop1(element->{
+            System.out.println(element);
+        });
+//        list1.remove(6);//无上一个结点
+//        list1.remove(5);//没有索引5这个结点
+        //带哨兵的单链表
+        System.out.println("带哨兵的单链表");
+        SinglyLinkedListSentinel list2 = new SinglyLinkedListSentinel();
+        list2.addFirst(1);
+        list2.addFirst(2);
+        list2.addFirst(3);
+        list2.addFirst(4);
+        list2.loop1(element->{
+            System.out.println(element);
+        });
+        System.out.println("------------------------一般循环遍历");
+        list2.loop2();
+        System.out.println("------------------------迭代遍历");
+        for(Integer i : list2) {
+            System.out.println(i);
+        }
+        System.out.println("------------------------尾插法测试");
+        //尾插法测试
+        SinglyLinkedList list3 = new SinglyLinkedList();
+        list3.addLast(1);
+        list3.addLast(2);
+        list3.addLast(3);
+        list3.addLast(4);
+        list3.loop1(element->{
+            System.out.println(element);
+        });
+        //查找测试
+        System.out.println("------------------------查找测试");
+        System.out.println(list3.findNode(2));;
+        System.out.println(list3.get(3));
+        //插入结点
+        System.out.println("------------------------插入结点");
+        list3.insert(3,10);
+        list3.insert(0,20);
+        list3.loop1(element->{
+            System.out.println(element);
+        });
+        //删除头结点
+        System.out.println("------------------------删除头结点");
+        list3.removeFirst();
+        list3.loop1(element->{
+            System.out.println(element);
+        });
+        //删除指定结点
+        System.out.println("------------------------删除指定结点");
+        list3.remove(1);
+        list3.loop1(element->{
+            System.out.println(element);
+        });
+        list3.remove(6);//无上一个结点
+        list3.remove(5);//没有索引5这个结点
     }
 }
