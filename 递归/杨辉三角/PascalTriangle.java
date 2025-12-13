a1a6398523641dc3962eb7af6a28ec6838ef9912
nwj <2936740697@qq.com>
2025-12-14 01:05:07 +0800

    perf(杨辉三角): 使用一维数组优化内存消耗
    优化杨辉三角的实现，从二维数组改为使用一维数组，减少内存使用

--- 递归/杨辉三角/PascalTriangle.java
+++ 递归/杨辉三角/PascalTriangle.java
@@ -30,6 +30,27 @@ public class PascalTriangle {
         }
     }
     public static void main(String[] args) {
-        print(5);
+        cprint(5);
+    }
+    //优化二：一维数组实现杨辉三角
+    public static void createRow(int[] row,int i){
+        if(i==0){
+            row[0]=1;
+            return;
+        }
+        for(int j=i;j>0;j--){
+            row[j]=row[j-1]+row[j];
+        }
+    }
+    public static void cprint(int n){
+        int[] row=new int[n];
+        for(int i=0;i<n;i++){
+            printspace(n,i);
+            createRow(row,i);
+            for(int j=0;j<=i;j++){
+                System.out.printf("%-4d",row[j]);
+            }
+            System.out.println();
+        }
     }
 }
