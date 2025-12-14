0ca019af88b7542ab9c5009186f4ea66cfaba98b
nwj <2936740697@qq.com>
2025-12-14 22:21:38 +0800

    refactor(链表反转): 重命名反转方法并添加递归实现
    将原有的reverse方法拆分为reverse1、reverse2和reverse3，分别对应不同的实现方式
    新增递归方式实现链表反转功能

--- 链表/单向链表/链表反向/LinkedListReverse.java
+++ 链表/单向链表/链表反向/LinkedListReverse.java
@@ -3,14 +3,14 @@ import 链表.单向链表.底层代码.SinglyLinkedList;
 
 public class LinkedListReverse {
     //方法一：创建新链表完成反转
-//    public static ListNode reverse(ListNode head) {
-//        ListNode n1=null;
-//        while(head!=null){
-//            n1=new ListNode(head.val,n1);
-//            head=head.next;
-//        }
-//        return  n1;
-//    }
+    public static ListNode reverse1(ListNode head) {
+        ListNode n1=null;
+        while(head!=null){
+            n1=new ListNode(head.val,n1);
+            head=head.next;
+        }
+        return  n1;
+    }
     //方法二：创建新链表但是不会创建新节点
     static class List{
         ListNode head;
@@ -29,7 +29,7 @@ public class LinkedListReverse {
             return removed;
         }
     }
-    public static ListNode reverse(ListNode head){
+    public static ListNode reverse2(ListNode head){
         List p1=new List(head);
         List p2=new List(null);
         while(true){
@@ -41,6 +41,16 @@ public class LinkedListReverse {
         }
         return p2.head;
     }
+    //方法三:递归方法实现链表反转
+    public static ListNode reverse3(ListNode head){
+        if(head==null||head.next==null){
+            return head;
+        }
+        ListNode newHead=reverse3(head.next);
+        head.next.next=head;
+        head.next=null;//递归的方式对节点5进行修改，让节点5指向节点4........
+        return newHead;//找到最后一个节点5
+    }
     public static void main(String[] args) {
         ListNode n1=null;
         ListNode n2=new ListNode(5,n1);
@@ -50,7 +60,7 @@ public class LinkedListReverse {
         ListNode n6=new ListNode(1,n5);
         System.out.println(n6);
 //        ListNode n7=new LinkedListReverse().reverse(n6);
-        ListNode n7=new LinkedListReverse().reverse(n6);
+        ListNode n7=new LinkedListReverse().reverse3(n6);
         System.out.println(n7);
     }
 }
