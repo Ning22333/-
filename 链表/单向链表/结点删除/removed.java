e2f8788c0582d0f05a6dd1a6e69bd489fda05c5b
nwj <2936740697@qq.com>
2025-12-15 22:05:40 +0800

    feat(链表): 添加删除倒数第n个节点功能
    新增递归方法实现删除链表倒数第n个节点的功能，并更新main方法进行测试

--- 链表/单向链表/结点删除/removed.java
+++ 链表/单向链表/结点删除/removed.java
@@ -13,7 +13,32 @@ public class removed {
             return p;
         }
     }
+    //删除倒数第index个结点
+    static public ListNode removebyreindex(ListNode head,int index){
+        ListNode n1=new ListNode(0,head);
+        resursion(n1,index);
+        return n1.next;
+
+    }
+    static public int resursion(ListNode p,int index){
+        if(p==null){
+            return 0;
+        }
+        int t=resursion(p.next,index);
+        if(t==index){
+            p.next=p.next.next;
+        }
+        return t+1;
+    }
+    
     public static void main(String[] args) {
-        
+        ListNode ls=new ListNode(8,null);
+        ls.next=new ListNode(7,null);
+        ls.next.next=new ListNode(6,null);
+        ls.next.next.next=new ListNode(5,null);
+        ls.next.next.next.next=new ListNode(7,null);
+        System.out.println(ls);
+        System.out.println(removeelement(ls,7));
+        System.out.println(removebyreindex(ls,2));
     }
 }
